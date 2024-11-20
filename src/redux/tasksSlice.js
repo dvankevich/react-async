35e6774943038962c2d4cfab2e63dcdb7c6ca5ad
dima <dvankevich@gmail.com>
2024-11-20 12:31:20 +0200

    createAsyncThunk

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,28 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Імпортуємо операцію
+import { fetchTasks } from "./operations";
+
 const tasksSlice = createSlice({
-  name: 'tasks',
+  name: "tasks",
   initialState: {
     items: [],
     isLoading: false,
     error: null,
   },
-  reducers: {
-    fetchInProgress(state) {
-      state.isLoading = true;
-    },
-    fetchSuccess(state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    fetchError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+  // Додаємо обробку зовнішніх екшенів
+  extraReducers: builder => {
+    builder
+      // eslint-disable-next-line no-unused-vars
+      .addCase(fetchTasks.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
-export const { fetchInProgress, fetchSuccess, fetchError } = tasksSlice.actions;
+// const tasksSlice = createSlice({
+//   name: 'tasks',
+//   initialState: {
+//     items: [],
+//     isLoading: false,
+//     error: null,
+//   },
+//   reducers: {
+//     fetchInProgress(state) {
+//       state.isLoading = true;
+//     },
+//     fetchSuccess(state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items = action.payload;
+//     },
+//     fetchError(state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//   },
+// });
+
+// export const { fetchInProgress, fetchSuccess, fetchError } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
