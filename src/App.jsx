cd943fb67de76cac4d0a125a48f7276084cb39c7
dima <dvankevich@gmail.com>
2024-11-20 11:44:27 +0200

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTasks } from './redux/operations';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasks } from "./redux/operations";
 
 export const App = () => {
   const dispatch = useDispatch();
+  // Отримуємо частини стану
   const { items, isLoading, error } = useSelector((state) => state.tasks);
-
+  // Викликаємо операцію
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
-
+  // Рендерим розмітку в залежності від значень у стані
   return (
     <div>
       {isLoading && <b>Loading tasks...</b>}
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import { fetchInProgress, fetchSuccess, fetchError } from './tasksSlice';
 
+// Встановлюємо базову URL-адресу
+// для всіх запитів axios
 axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 
 export const fetchTasks = () => async (dispatch) => {
   try {
+    // Індикатор завантаження
     dispatch(fetchInProgress());
     const response = await axios.get('/tasks');
+    // Обробка даних
     dispatch(fetchSuccess(response.data));
   } catch (e) {
+    // Обробка помилки
     dispatch(fetchError(e.message));
   }
 };
